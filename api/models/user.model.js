fe532de11bb9d628c6eb813eec1ca4efbd38dcfa
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-04-09 18:52:40 +0700

    create sign up API route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from "mongoose";
 
-const userSchema = new Mongoose.scema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
