0610b65fe8f10dadcd650672e36eff628b6f01f3
Ishaan Vashishth <ishaan.vashishth@csdevents.com>
2024-04-18 21:57:43 +0700

    add delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,15 @@
 import { useSelector } from "react-redux"
 import {useState, useRef, useEffect} from 'react';
-import {getDownloadURL, getStorage,ref, uploadBytesResumable } from 'firebase/storage';
+import {getDownloadURL, 
+  getStorage,ref,
+   uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
-import { updateUserStart,updateUserSuccess,updateUserFailure } from "../redux/user/userSlice";
+import { updateUserStart,
+  updateUserSuccess,
+  updateUserFailure, 
+  deleteUserFailure,
+   deleteUserStart, 
+   deleteUserSuccess} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -72,6 +79,23 @@ export default function Profile() {
       dispatch(updateUserFailure(error.message));
     }
   };
+
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch (`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
   return (
 
     
@@ -103,7 +127,7 @@ export default function Profile() {
           {loading? 'Loading...' : 'update'}</button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'> delete account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'> delete account</span>
         <span className='text-red-700 cursor-pointer'> sign out</span>
       </div>
       <p className="text-red-700 mt-5"> {error? error: ''}</p>
